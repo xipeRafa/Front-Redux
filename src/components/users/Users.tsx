57c9a7215226d168e9f03a9ab8cd94666ed6d138
fer <fer@fers-iMac.local>
2025-01-18 14:15:38 -0700

    koko

---
+++ src/components/users/Users.tsx
@@ -0,0 +1,134 @@
+
+import { useEffect } from 'react'
+import { PostForm } from './PostForm';
+import { useUsers } from '../../hooks'
+
+export const Users = () => {
+
+    const usersListCSS = {
+        display: "block",
+        border: "2px solid salmon",
+        padding: "10px",
+        width: "400px",
+        marginLeft: "50px",
+        marginBottom: "10px",
+        backgroundColor: "lightgray"
+    }
+
+
+    const { dataUsersGet, users, deleteUser, postUser, switchUser, setInfoToForm,
+            editMode, newDataEdit, defaultModeEdith, uploadUserImg, usersFinder, 
+            paginationSelect, paginationNext } = useUsers()
+
+
+console.log(users)
+
+    useEffect(() => {
+        dataUsersGet()
+    }, [])
+
+
+    const handleDelete = (el: Object) => {
+        deleteUser(el)
+    }
+
+    const handleSwitch = (el) => {
+        switchUser(el)
+    }
+
+    const handleEdith = (el: String) => {
+        setInfoToForm(el)
+    }
+
+
+    const handlePaginationSelect=(ps)=>{
+        let step = Number(ps)
+        paginationSelect(step)
+    }
+
+
+    return (
+        <div>
+
+
+            <h3 className='mx-5'>Usuarios</h3>
+
+
+
+            <div className='mx-5 my-2'>
+
+                <div>{`Usuarios del: ${localStorage.step-(localStorage.step-1)} para ${localStorage.step}, en Total: ${localStorage.UsersTotal}`}</div>
+
+                <input type="button" value='Previous' onClick={()=>paginationNext(false)} className='btn btn-secondary'/>
+
+                {JSON.parse(localStorage.UsersArray)?.map((el, i) => (
+                    i < localStorage.UsersTotal/localStorage.step &&
+                        <input key={i+'<^>'} type="button" value={i+1} onClick={()=>handlePaginationSelect((i+1)*localStorage.step)} className='btn btn-secondary'/> 
+                ))}
+           
+                <input type="button" value='Next' onClick={()=>paginationNext(true)} className='btn btn-secondary'/>
+
+                <select className='form-select col-12 my-2' style={{width:'200px'}} onChange={(e)=>handlePaginationSelect(e.target.value)}>
+                    {JSON.parse(localStorage.UsersArray)?.map((el, i) => (
+                        i < localStorage.UsersTotal/localStorage.step && 
+                            <option key={i} value={(i+1)*localStorage.step}>   {`${((i+1)*localStorage.step)-(localStorage.step-1)} a ${(i+1)*localStorage.step}`}   </option>
+                    ))}
+                </select>
+            </div>
+
+
+
+
+
+
+            <PostForm postUser={postUser} editMode={editMode} newDataEdit={newDataEdit} defaultModeEdith={defaultModeEdith} />
+
+
+
+
+
+            <input type="search" className='form-control col-12 my-2 mx-5' style={{width:'200px'}} placeholder='Buscar Usuarios' onChange={(e)=> usersFinder(e.target.value.trim())} />
+
+
+
+
+            {users.usuarios?.length < 3 &&  
+                <div className='mx-5 my-4'>
+                    <button onClick={()=>handlePaginationSelect(localStorage.step)} className='btn btn-info'> Click para Ver todos los Usuarios</button>
+                </div>
+            }
+ 
+
+
+            {users.usuarios?.map((el, i) => (
+                <div key={i + '!@#'} style={usersListCSS}>
+
+                    <h3>Nombre: {el.nombre}</h3>
+                    <p>Mail:   {el.correo}</p>
+
+                    <p>Role:   {el.rol}</p>
+                    <p>id:     {el.uid}</p>
+
+                    <p>State:  {el.estado ? ' true' : ' false'}</p>
+                    <p>Google: {el.google ? ' true' : ' false'}</p>
+
+                    <img src={el.img} width='100px' />
+                    <p>Toggle: {el.toggle ? ' true' : ' false'} </p>
+
+                    <button onClick={() => handleDelete(el)}>Eliminar</button>
+                    <button onClick={() => handleSwitch(el)}>Toggle</button>
+                    <button onClick={() => handleEdith(el)}>Edit</button>
+                    <input type="file" id="file-upload" onChange={(e) => uploadUserImg(el.uid, e.target.files[0])} />
+
+                </div>
+            ))}
+
+
+
+
+
+
+
+        </div>
+    )
+}
